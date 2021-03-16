d63625d53f82d59e1d42fc82005588ce62a0da9b
JulienJ-44 <74324167+JulienJ-44@users.noreply.github.com>
2021-03-16 15:00:31 +0100

    Création code dataviz

---
+++ Codes/0_Dataviz.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 16 14:56:04 2021
+
+@author: Lenovo
+"""
+
+import matplotlib.pyplot as plt
+%matplotlib inline 
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
+
+#Création dictionnaire de correspondance entre code et désignation des classes
+tab_designation = {2583:'PISCINE & ACCESSOIRES',1560:'MAISON: cusine, mobiliers',1300:'JOUETS RADIO COMMANDES'
+,2060:'LUMINAIRES, DECO',2522:'FOURNITURE de BUREAU',1280:'JOUETS ENFANTS',2403:'LIVRES & MAGASINES: histoire, musées'
+,2280:"REVUES d'ARTS et SPECTACLES",1920:"LITERIE, TEXTILE MAISON",1160:"CARTES à COLLECTIONNER",
+1320:"ACCESSOIRES BEBES",10:"LITTERATURE/ROMANS (occasions?)",2705:"ROMAN: livre histoire?",1140:"FIGURINES MANGAS"
+,2582:"MOBILIER & ACCESSOIRES extérieur",40:"JEUX VIDEOS (import?)",2585:"OUTILS JARDINS",1302:"LOISIRS/EQUIPEMENT extérieur"
+,1281:"JEUX EDUCATIFS ENFANTS",50:"ACCESSOIRES CONSOLES de JEUX",2462:"JEUX VIDEOS OCCASIONS",2905:"JEUX PC",
+60:"CONSOLES de JEUX",2220: "ACCESSOIRES ANIMAUX",1301:"VETEMENTS ENFANTS/BEBE",1940:"ALIMENTATIONS, CONFISERIES, CAFE",1180:"JEUX DE GUERRE sur TABLE"}
+
+classes_codes=sorted(classes_codes)
+#on étudie le champ description
+colonneClass = "description" 
+df_results = pd.DataFrame(columns=['2chiffres+','poids','longueur','volume','ans_mois','N°','pourcent','pièces'])#, 'date','octets','dimension'
+
+class_text_cols = []
+#on parcourt la liste des classes
+for class_code in classes_codes:
+  #on copie dans un df temporaire les lignes correspondant au code de classe actuel
+  df_current_class = df[df["prdtypecode"]==class_code]
+  #affichage des infos de la classe
+  #print("Classe:", class_code, "\n", len(df_current_class.index), " éléments")
+  nb_total = len(df_current_class.index)
+  #nb d articles contenant la regexp / nb d articles total dans la classe
+  nb_occur1=(len(df_current_class[df_current_class["desc_nb2chiffres+"] > 0]))/nb_total
+  nb_occur4=(len(df_current_class[df_current_class["desc_poids"] > 0]))/nb_total
+  nb_occur5=(len(df_current_class[df_current_class["desc_long"] > 0]))/nb_total
+  nb_occur7=(len(df_current_class[df_current_class["desc_vol"] > 0]))/nb_total
+  #nb_occur2=(len(df_current_class[df_current_class["desc_Go"] > 0]))/nb_total
+  nb_occur3=(len(df_current_class[df_current_class["desc_num"] > 0]))/nb_total
+  nb_occur12=(len(df_current_class[df_current_class["desi_num"] > 0]))/nb_total
+  nb_occur8=(len(df_current_class[df_current_class["desc_ans_mois"] > 0]))/nb_total
+  nb_occur9=(len(df_current_class[df_current_class["desc_pieces"] > 0]))/nb_total
+  nb_occur11=(len(df_current_class[df_current_class["desc_pourcent"] > 0]))/nb_total
+  new_row = pd.Series(data={'2chiffres+':nb_occur1,
+                            #,'dimension':nb_occur6, 'date':nb_occur10 'octets':nb_occur2,
+                            'poids':nb_occur4,'longueur':nb_occur5,'volume':nb_occur7
+                            ,'ans_mois':nb_occur8,'N°(desi)':nb_occur12,'N°':nb_occur3,'pièces':nb_occur9,'pourcent':nb_occur11}
+                      , name=str(class_code)+ "-"+tab_designation[class_code])
+  df_results = df_results.append(new_row, ignore_index=False)
+
+
+plt.figure(figsize=(25,12))
+sns.heatmap(df_results, annot=True, cmap='YlGnBu');
+p = plt.title("Fréquences des différences unités de mesures au sein de chacune des classes");
+images_dir = '/Drive/My Drive/Colab Notebooks'
+plt.savefig(f"{images_dir}/Frequence_unites_par_classe.png")
--- Codes/1_Cleaning data set.py
+++ Codes/1_Cleaning data set.py
@@ -2,6 +2,86 @@
 """
 Created on Tue Mar 16 14:21:09 2021
 
-@author: Lenovo
+@author: Rakuteam
 """
+
+#CONNEXION à google drive TODO: adapter à environnement GIThub Streamlit
+import pandas as pd
+from google.colab import drive
+
+drive.mount('/Drive')
+
+#CHARGEMENT des fichiers
+import re  
+X = pd.read_csv("/Drive/My Drive/Colab/X_train_update.csv", index_col=0) 
+#X_train_update.info()
+y = pd.read_csv("/Drive/My Drive/Colab/Y_train_CVw08PX.csv", index_col=0) 
+#stockage de tous les codes de classe dans une liste
+classes_codes = (y['prdtypecode'].value_counts().index.tolist())
+#PREPARATION: FUSION de X et y et remplacement des VALEURS MANQUANTES par ""
+df1 = X.drop(['imageid'], axis = 1)#
+#on joint X et y
+df = pd.merge(df1, y, left_index=True, right_index=True)
+df['designation']=df['designation'].fillna("")
+
+#valeurs MANQUANTES
+df['description']=df['description'].fillna("")
+#turn all text to LOWER case
+df['description'] = df['description'].str.lower()
+df['designation'] = df['designation'].str.lower()
+
+#recodage des caractères HTML
+df.replace(["&eacute;","&egrave;","&euml;","&ecirc;","&ocirc;","&ugrave;","&ccedil;","&agrave;","&nbsp;","&amp;","&#39;"]
+           , ["é","è","ë","ê","ô","ù","ç","à"," ","&","'"], inplace=True, regex=True)
+
+#df=df.head(20)
+"""
+Return the text in lower case after removing html tags
+"""    
+def pre_process(text):
+    # lowercase
+    #text=text.lower()
+    #remove tags
+    text=re.sub('<.*?>'," ",text) 
+    return text
+
+df['description'] = df['description'].apply(lambda x:pre_process(x))
+df['designation'] = df['designation'].apply(lambda x:pre_process(x))
+
+
+df.info()
+
+#FONCTION pour supprimer les mots inutiles
+# Download the stopwords
+from nltk.tokenize import PunktSentenceTokenizer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import nltk as nltk
+nltk.download('punkt') #télécharge les paquets language (dont FR)
+nltk.download('stopwords')
+
+#Ajoute des stopwords à la liste française prédéfinie
+stop_words = set(stopwords.words('french'))
+stop_words.update(stopwords.words('english'))
+stop_words.update(stopwords.words('german'))
+stop_words.update(['-','de','en','pour','la','(',')','the','/li','/strong','strong','and','non','eacute','comme','cette','of'])
+
+def filtrer_mots_inutiles(texte):
+  """
+  Fonction pour transformer un texte en une suite de mots séparés par des espaces
+  et en excluant les stopwords et les mots de moins de 2 caractères
+  """
+  mots = word_tokenize(texte, language='french')
+  tokens = []
+  for mot in mots:
+    if (mot not in stop_words and len(mot)>1):
+      tokens.append(mot.lower())
+  return tokens
+
+df['designation']= df['designation'].apply(lambda x: filtrer_mots_inutiles(x))
+df['description']= df['description'].apply(lambda x: filtrer_mots_inutiles(x))
+
+#Création d'un fichier CSV sur Google Drive
+path = '/Drive/My Drive/Projet Rakuten'
+df.to_csv(f'{path}/dataset_cleaned.csv')
 

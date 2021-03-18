4e480c89938c5f494cd2d994e3532dfecb0c7eb9
psynapsemng <78550822+psynapsemng@users.noreply.github.com>
2021-03-18 12:26:23 +0100

    Ajout script pour multi appli Streamlit

---
+++ Streamlit_rakuten/app4.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 18 09:24:15 2021
+
+@author: slam_
+"""
+
+
+import streamlit as st
+     
+# app3.py
+def app():
+    st.title('Démarche')
+    st.write('Workflow')

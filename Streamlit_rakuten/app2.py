d0cf7db13296285faf657ff735440e6e53036a7b
damienld <dledirach@yahoo.fr>
2021-03-22 19:34:32 +0100

    app2

--- Streamlit_rakuten/app2.py
+++ Streamlit_rakuten/app2.py
@@ -17,7 +17,9 @@ import numpy as np
 
 import seaborn as sns
 import matplotlib.pyplot as plt
-from load_functions import *
+#import plotly.graph_objects as go
+
+from modelisation import *
 
 Models = ["Modèle RF", 
          "Modèle NN Text", 
@@ -30,99 +32,28 @@ def app():
     st.radio("Selection du modèle",(Models))
     
 
- 
 alg = ['1-RF', '2-CNN image', '3-DNN texte', 'weighted voting']
 classifier = st.selectbox('Selection du modèle', alg)
 if classifier=='1-RF':
+    model_index = "1"
 
-    model_index = 1
-    
-    def get_classif_report(index_model):
-        model_index=index_model
-        y_test=get_ytest()
-        y_train=get_ytrain()
-        model_selected=set_model_name(index_model)
-        y_pred_proba=calc_y_pred(index_model)
-        #preparation des données pour le crosstab
-        # Convertir Dataframe en array
-        y_pred_proba_arr = y_pred_proba.to_numpy()
-        y_test = y_test.to_numpy()
-        # on prend l'index de la proba la + élevée
-        # pour récupérer les classes
-        y_pred = y_pred_proba_arr.argmax(axis=1)
-        y_pred
-        # Pour ajouter une dimension en plus
-        y_pred = np.reshape(y_pred, (-1, 1))
-        return(metrics.classification_report(y_test, y_pred))
-    
-        print("Evaluation détaillée de la Classification :\n \n" , get_classif_report("1"))
-     
 elif classifier == '2-CNN image':
 
-    model_index = 2
-    
-    def get_classif_report(index_model):
-        model_index=index_model
-        y_test=get_ytest()
-        y_train=get_ytrain()
-        model_selected=set_model_name(index_model)
-        y_pred_proba=calc_y_pred(index_model)
-        #preparation des données pour le crosstab
-        # Convertir Dataframe en array
-        y_pred_proba_arr = y_pred_proba.to_numpy()
-        y_test = y_test.to_numpy()
-        # on prend l'index de la proba la + élevée
-        # pour récupérer les classes
-        y_pred = y_pred_proba_arr.argmax(axis=1)
-        y_pred
-        # Pour ajouter une dimension en plus
-        y_pred = np.reshape(y_pred, (-1, 1))
-        return(metrics.classification_report(y_test, y_pred))
-    
-        print("Evaluation détaillée de la Classification :\n \n" , get_classif_report("1"))
-    
+    model_index = "2"
+
 elif classifier == 'DNN texte':
-    model_index = 3
-    
-    def get_classif_report(index_model):
-        model_index=index_model
-        y_test=get_ytest()
-        y_train=get_ytrain()
-        model_selected=set_model_name(index_model)
-        y_pred_proba=calc_y_pred(index_model)
-        #preparation des données pour le crosstab
-        # Convertir Dataframe en array
-        y_pred_proba_arr = y_pred_proba.to_numpy()
-        y_test = y_test.to_numpy()
-        # on prend l'index de la proba la + élevée
-        # pour récupérer les classes
-        y_pred = y_pred_proba_arr.argmax(axis=1)
-        y_pred
-        # Pour ajouter une dimension en plus
-        y_pred = np.reshape(y_pred, (-1, 1))
-        return(metrics.classification_report(y_test, y_pred))
-    
-        print("Evaluation détaillée de la Classification :\n \n" , get_classif_report("1"))
-    
+    model_index = "3"
+
 elif classifier == 'weighted voting':
-    model_index = 4
+    model_index = "4"
     
-    def get_classif_report(index_model):
-        model_index=index_model
-        y_test=get_ytest()
-        y_train=get_ytrain()
-        model_selected=set_model_name(index_model)
-        y_pred_proba=calc_y_pred(index_model)
-        #preparation des données pour le crosstab
-        # Convertir Dataframe en array
-        y_pred_proba_arr = y_pred_proba.to_numpy()
-        y_test = y_test.to_numpy()
-        # on prend l'index de la proba la + élevée
-        # pour récupérer les classes
-        y_pred = y_pred_proba_arr.argmax(axis=1)
-        y_pred
-        # Pour ajouter une dimension en plus
-        y_pred = np.reshape(y_pred, (-1, 1))
-        return(metrics.classification_report(y_test, y_pred))
-    
-print("Evaluation détaillée de la Classification :\n \n" , get_classif_report("1"))
+st.text(get_classifreport(model_index))
+
+cross=get_crosstab("3")
+
+fig, axes = plt.subplots(1,1, sharex=True)
+fig.suptitle("Heatmap")
+plt.figure(figsize=(25,25))
+sns.heatmap(cross,  annot=True, cmap="YlGnBu");
+plt.xticks(rotation=90);
+#st.plotly_chart(fig, use_container_width=True, figsize=(12,8))

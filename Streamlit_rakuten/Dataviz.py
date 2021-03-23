f4e5c57bf91b039b50fa956070ce309581be8aaa
JulienJ-44 <74324167+JulienJ-44@users.noreply.github.com>
2021-03-23 21:35:39 +0100

    Dataviz

--- Streamlit_rakuten/Dataviz.py
+++ Streamlit_rakuten/Dataviz.py
@@ -8,11 +8,30 @@ Created on Tue Mar 23 11:17:29 2021
 import requests
 import io
 import pandas as pd
+from modelisation import *
 
 
 def get_dataset_cleaned():
-  url = "https://raw.githubusercontent.com/JulienJ-44/rakuteam/main/Datasets/dataset_cleaned.csv"# Make sure the url is the raw version of the file on GitHub
+  url = "https://raw.githubusercontent.com/JulienJ-44/rakuteam/main/Features/data_features_final.csv"# Make sure the url is the raw version of the file on GitHub
   download = requests.get(url).content
   dataset_cleaned = pd.read_csv(io.StringIO(download.decode('utf-8')))
   # Remplacer les labels de 0 à 26
   return dataset_cleaned
+
+def get_regex_value_counts(val_regex):
+    df_code = load_df_code_designation(1)
+    df = get_dataset_cleaned()
+    df = df[df['designation'].notnull()]
+    df_1 = df[df['designation'].str.contains(val_regex)]
+    class_nb = pd.DataFrame(df_1['prdtypecode_x'].value_counts())
+    class_nb.columns = ["Nombre d'articles"]
+    test = class_nb.join(df_code)
+    return test
+
+def get_pixel_value_counts(x_2):
+    df_code = load_df_code_designation(1)
+    df = get_dataset_cleaned()
+    class_pix_mean = pd.DataFrame(df.groupby(by='prdtypecode_x')[x_2].mean().round(2)*100)
+    class_pix_mean.columns = ["Pourcentage moyen de pixels"]
+    test = class_pix_mean.join(df_code)
+    return class_pix_mean
--- Streamlit_rakuten/app3.py
+++ Streamlit_rakuten/app3.py
@@ -41,20 +41,70 @@ def app():
     
     
     #créer la table de répartition des classe et 
-    class_nb = pd.DataFrame(df['prdtypecode'].value_counts())
-    class_nb.columns = ["Nombre d'articles"]
-    test = class_nb.join(df_desig_class)
+    # class_nb = pd.DataFrame(df['prdtypecode_x'].value_counts())
+    # class_nb.columns = ["Nombre d'articles"]
+    # test = class_nb.join(df_desig_class)
     
-    fig = plt.figure(figsize=(27,10))
+    
+    saisie_manuelle = st.text_input("Entrer le mot à chercher")
+    
+    PAGES = {
+    "Pas de motif particulier (tous les articles)": '.',
+    "n°,N° ou numéro": '(numéro )',
+    "Unités de longueur (m, cm, mm)": '([\d.]+)\s?(cm|mm|m|M)[\s.]',
+    "Unités de poids (kg, g, mg)": '([\d.]+)\s?(cm|mm|m|M)[\s.]',
+    "Unités de volume (l, cl, ml)": '([\d.]+)\s?(cm|mm|m|M)[\s.]',
+    "Saisie manuelle": saisie_manuelle
+    }
+
+    
+    
+    selection = st.radio("Sélectionner le motif à rechercher", list(PAGES.keys()))
+    x = PAGES[selection]
+    
+    
+    # x = '(numéro )'
+
+    test = get_regex_value_counts(x)
+    
+    fig = plt.figure(figsize=(50,18))
     # plt.subplot(1,2,1)
     
     # g = sns.barplot(data=test, x="Classe - Désignation", y="Nombre d'article")
     # g.set_xticklabels(g.get_xticklabels(),rotation=90);
     # # print(test)
-    
+    sns.set(font_scale=3) 
     # plt.subplot(1,2,2)
     sns.barplot(data=test, y="désignation", x="Nombre d'articles", orient='h');
     # h.set_xticklabels(g.get_xticklabels(),rotation=90);
     st.pyplot(fig)
     
-   
+    PAGES_2 = {
+    "Moyenne de nombre de pixel 'Red'": 'R',
+    "Moyenne de nombre de pixel 'Green'": 'G',
+    "Moyenne de nombre de pixel 'Blue'": 'B',
+    "Moyenne de nombre de pixel 'Black'": 'noir',
+    "Moyenne de nombre de pixel 'White'": 'blanc',
+    }
+
+    
+    
+    selection_2 = st.radio("Sélectionner le motif à rechercher", list(PAGES_2.keys()))
+    x_2 = PAGES_2[selection_2]
+     
+
+    test_2 = get_pixel_value_counts(x_2)
+    
+    st.dataframe(test_2)
+    
+    fig_2 = plt.figure(figsize=(50,18))
+    # plt.subplot(1,2,1)
+    
+    # g = sns.barplot(data=test, x="Classe - Désignation", y="Nombre d'article")
+    # g.set_xticklabels(g.get_xticklabels(),rotation=90);
+    # # print(test)
+    sns.set(font_scale=3) 
+    # plt.subplot(1,2,2)
+    # sns.barplot(data=test_2, y="désignation", x="Pourcentage moyen de pixels", orient='h');
+    # h.set_xticklabels(g.get_xticklabels(),rotation=90);
+    st.pyplot(fig_2)

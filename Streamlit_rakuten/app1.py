446a1371f4d654ab16bd44892e7ac9bcf936cd9d
damienld <dledirach@yahoo.fr>
2021-03-24 14:33:55 +0100

    update app1.py

--- Streamlit_rakuten/app1.py
+++ Streamlit_rakuten/app1.py
@@ -17,6 +17,22 @@ import cv2
 import numpy as np
 import streamlit as st
 import pandas as pd
+
+desiinit="jeu chaise longue pcs textilène noir noir"
+descinit="cet ensemble deux chaises longues haute qualité petite table assortie idéal passer après-midi détente jardin camping chaises longues durables faciles nettoyer revêtues textilène doux confortable construites cadre acier robuste deux chaises longues d'extérieur durables résistants intempéries l'ensemble complété table assortie élégant dessus table verre lequel pouvez mettre boissons garder livre téléphone portée main cet ensemble excellent ajout espace vie extérieur couleur noir matériau chaise longue structure acier 43 siège dossier textilène dimensions chaise longue 200 58 32 cm dimensions table 30 30 295 cm hauteur dossier réglable 62/72/80/89/95 cm comprend table dessus table verre mm d'épaisseur résistance intempéries matériel polyester 30 pvc 70"
+"""desi=desiinit
+descr=descinit
+dfcleaned=clean_manualdata(desi,descr)
+print("RF")
+weightRF=0.73
+img="test.jpg"
+df=add_features_to_manualdf(dfcleaned, img)
+df=df.fillna(0)
+df_rf=df.drop(["prdtypecode","designation","description","productid","best_idf"], axis=1)
+ypred_proba_RF=RF_predict(True, df_rf)
+df_ypred_proba_RF = pd.DataFrame(ypred_proba_RF).T
+df_ypred_proba_RF=df_ypred_proba_RF.sort_index(axis=0)
+print (ypred_proba_RF)"""
 #df=clean_manualdata("bébé","")
 #df=add_features_to_manualdf(df)
 
@@ -33,8 +49,6 @@ import pandas as pd
 #df_keywords.head(15)
 #article 77312 / 2582
 #y_predproba DNN:3.510231e-09,1.3902971e-16,3.0522055e-15,5.5146954e-09,5.431842e-08,3.6039685e-13,4.7371586e-06,0.9474246,0.051802292,1.4928757e-07,0.00032929692,5.3290102e-14,1.42541e-13,1.1241913e-06,1.3613869e-09,2.1363553e-10,5.1120828e-09,1.5232825e-17,9.299475e-06,1.6853333e-12,4.857436e-13,2.281767e-05,0.00038954898,1.3751665e-05,8.9084694e-12,1.671928e-06,7.500114e-07
-desiinit="jeu chaise longue pcs textilène noir noir"
-descinit="cet ensemble deux chaises longues haute qualité petite table assortie idéal passer après-midi détente jardin camping chaises longues durables faciles nettoyer revêtues textilène doux confortable construites cadre acier robuste deux chaises longues d'extérieur durables résistants intempéries l'ensemble complété table assortie élégant dessus table verre lequel pouvez mettre boissons garder livre téléphone portée main cet ensemble excellent ajout espace vie extérieur couleur noir matériau chaise longue structure acier 43 siège dossier textilène dimensions chaise longue 200 58 32 cm dimensions table 30 30 295 cm hauteur dossier réglable 62/72/80/89/95 cm comprend table dessus table verre mm d'épaisseur résistance intempéries matériel polyester 30 pvc 70"
 
 
 def app():
@@ -52,21 +66,23 @@ def app():
     # creating a file uploader 
     #file_input="test2.jpg"#for testing only
     file_input = st.file_uploader("Choose a image file", type="jpg")
-    if file_input is not None: """
+    img=""
+    if file_input is not None: 
         # Convert the file to an opencv image.
         file_bytes = np.asarray(bytearray(file_input.read()), dtype=np.uint8)
         opencv_image = cv2.imdecode(file_bytes, 1)
-    
-        # Afficher l'image:
         st.image(opencv_image, channels="BGR")
         
-        df=clean_manualdata(descr,desi)
-        df=add_features_to_manualdf(df,file_input)
+        #df=clean_manualdata(descr,desi)
+        #df=add_features_to_manualdf(df,file_input)
         #img="test2.jpg"
-        add_imgfeatures(df, file_input)
-        
-        st.dataframe(df)"""
-    
+        #add_imgfeatures(df, file_input)
+
+        #st.dataframe(df)         
+        #img=file_input#[name]
+
+    #st.text(img)
+
     if st.button("Chercher"):
     #    desi=desiinit
     #descr=descinit
@@ -75,6 +91,7 @@ def app():
         weightRF=0.73
         #img="test2.jpg"
         df=add_features_to_manualdf(dfcleaned, file_input)
+        #st.dataframe(df)   
         df=df.fillna(0)
         df_rf=df.drop(["prdtypecode","designation","description","productid","best_idf"], axis=1)
         ypred_proba_RF=RF_predict(True, df_rf)
@@ -105,7 +122,9 @@ def app():
         df_ypred_proba=df_ypred_proba.drop("index",axis=1)
         #df_ypred_proba.sort()
         #print(df_ypred_proba.head(5))
+        st.write("Probabilités des différents modèles par classe")
         st.dataframe(df_ypred_proba)
         classe_code_best_proba=int(df_ypred_proba.iloc[0,1])
         df_keywords=display_keywords_fromclasscodes(classe_code_best_proba)#,2583)
-        st.dataframe(df_keywords.head(15))
+        st.write("Mots-clés de la classe prédite")
+        st.dataframe(df_keywords)

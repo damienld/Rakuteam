f27aa477f03f50d9484070e1319532f77e76a032
damienld <dledirach@yahoo.fr>
2021-03-24 18:30:01 +0100

    Create sample.py

---
+++ Streamlit_rakuten/sample.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 24 15:24:15 2021
+
+@author: Dam
+"""
+import requests
+import io
+import random
+import pandas as pd
+def get_random_article(classe=-1, nbrows=1):
+    url = "https://raw.githubusercontent.com/JulienJ-44/rakuteam/main/Datasets/dataset_streamlit.csv"
+    download = requests.get(url).content
+    df=pd.read_csv(io.StringIO(download.decode('utf-8')), index_col=0)
+    if (classe >= 0):
+        df=df[df["prdtypecode"]==classe]
+    listchoix=random.sample(range(len(df)), nbrows)
+    print(listchoix)
+    df=df.iloc[listchoix]
+    print("longueur ", len(df))
+    for i in range(len(df)):
+        print(i)
+        path="/images/"
+        img = df.iloc[i,5]
+        df.iloc[i,5]=path+img
+    return df
+
+#df=get_random_article(2583, 3)
+#print(df.head())
+"""
+print(df.head(1))
+import shutil
+for i in range(len(df)):
+   
+  path="D:/DataScientest/images/images/image_train/"
+  img = df.iloc[i,5]
+  
+  img=path+img
+  filePath = shutil.copy(img,'C:/Users/Dam/Documents/GitHub/rakuteam/Streamlit_rakuten/image"s')
+"""

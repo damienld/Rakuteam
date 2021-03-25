e09eaae035947319baf2ec36b8de6bac01306d2f
JulienJ-44 <74324167+JulienJ-44@users.noreply.github.com>
2021-03-25 16:08:57 +0100

    Présentation

--- Streamlit_rakuten/app4.py
+++ Streamlit_rakuten/app4.py
@@ -10,15 +10,22 @@ import streamlit as st
      
 # app3.py
 def app():
-    st.title('Démarche')
-    st.write('Workflow')
-    
+
+    st.write('')
     st.image("./présentation/01.PNG")
+    st.write('')
     st.image("./présentation/02.PNG")
+    st.write('')
     st.image("./présentation/03.PNG")
+    st.write('')
     st.image("./présentation/04.PNG")
+    st.write('')
     st.image("./présentation/05.PNG")
+    st.write('')
     st.image("./présentation/06.PNG")
+    st.write('')
     st.image("./présentation/07.PNG")
+    st.write('')
     st.image("./présentation/08.PNG")
+    st.write('')
     st.image("./présentation/09.PNG")

e0b5b514e2a33a6747c836ad0f651d0d331f8153
JulienJ-44 <74324167+JulienJ-44@users.noreply.github.com>
2021-03-27 15:31:18 +0100

    Transformation graphiques bokeh

--- Streamlit_rakuten/app3.py
+++ Streamlit_rakuten/app3.py
@@ -12,6 +12,11 @@ import pandas as pd
 from Dataviz import *
 from modelisation import *
 import seaborn as sns
+from bokeh.plotting import figure, output_notebook, show 
+from bokeh.models.tools import HoverTool
+from bokeh.models import ColumnDataSource
+from bokeh.transform import factor_cmap
+from bokeh.palettes import Spectral6
 
 df = get_dataset_cleaned()
 df_desig_class = load_df_code_designation(1)
@@ -41,52 +46,100 @@ def app():
     x = PAGES[selection]
     
   
-    test = get_regex_value_counts(x)
+    regex = get_regex_value_counts(x)
+    regex = regex.sort_values("Nombre d'articles",ascending = True)
+
+    desi = regex["désignation"]
     
-    fig = plt.figure(figsize=(50,18))
- 
-    sns.set(font_scale=3) 
+    counts = regex["Nombre d'articles"]
+        
+    hover1 = HoverTool(
+            tooltips=[
+                ("Nombre d'articles", "@right")
+               ])
+
+    # Instanciation de la figure
     
-    sns.barplot(data=test, y="désignation", x="Nombre d'articles", orient='h');
+    p1 = figure(y_range = desi,                         # l'axe des ordonnées devient un axe catégoriel
+               plot_width = 900, plot_height = 400,      # dimensions de la figure
+               x_axis_label="Nombre d'articles", y_axis_label="Désignation de la classe") 
     
-    plt.ylabel("Désignation de la classe d'articles");
+    # Instanciation d'un diagramme à barres horizontales
+    p1.add_tools(hover1)
     
-    plt.xlabel("Nombre d'atricles avec au moins une occurence du motif recherché");
+    p1.hbar(y = desi,                    # ordonnées
+           right = counts,  # abscisses
+           height = 0.7,
+           fill_color="#9b59b6",
+           line_color='black')                   # épaisseur
     
-    st.pyplot(fig)
+    # Affichage de la figure
+    
+    st.bokeh_chart(p1)
     
     PAGES_2 = {
     "Moyenne de nombre de pixel 'Red'": 'R',
     "Moyenne de nombre de pixel 'Green'": 'G',
     "Moyenne de nombre de pixel 'Blue'": 'B',
-    "Pourcentage moyen de pixel 'Black'": 'noir',
+    #"Pourcentage moyen de pixel 'Black'": 'noir',
     "Pourcentage moyen de pixel 'White'": 'blanc',
     }
-
     
+    couleur = {
+    'R': '#e74c3c',
+    'G': '#2ecc71',
+    'B': '#5499c7',
+    'noir': '#1b2631',
+    'blanc': '#ecf0f1',
+    }
     
     selection_2 = st.radio("Sélectionner le motif à rechercher", list(PAGES_2.keys()))
     x_2 = PAGES_2[selection_2]
-    test_2 = get_pixel_value_counts(x_2)
-    # st.dataframe(test_2)
-    fig_2 = plt.figure(figsize=(50,18))
-    sns.set(font_scale=3) 
+    
+    print(x_2)
+    
+    pixel = get_pixel_value_counts(x_2)
+    
+    pixel = pixel.sort_values("Pourcentage moyen de pixels",ascending = True)
+    
     if x_2 == 'blanc' or x_2 == 'noir':
       x_name = 'Pourcentage moyen de pixels '+ x_2
+      counts = pixel["Pourcentage moyen de pixels"]*100
     else:
       x_name = 'Nombre moyen de pixels '+ x_2
-      
-    sns.barplot(data=test_2, y="désignation", x='Pourcentage moyen de pixels', orient='h');
+      counts = pixel["Pourcentage moyen de pixels"]
     
-    plt.ylabel("Désignation de la classe d'articles");
+    desi = pixel["désignation"]
     
-    plt.xlabel(x_name);
+    hover2 = HoverTool(
+            tooltips=[
+                (x_name, "@right")
+               ])
+
+    # Instanciation de la figure
     
-    st.pyplot(fig_2)
+    p2 = figure(y_range = desi,                   # l'axe des ordonnées devient un axe catégoriel
+               plot_width = 900, plot_height = 400,      # dimensions de la figure
+               x_axis_label=x_name, y_axis_label="Désignation de la classe")
+    
+    p2.add_tools(hover2)
+    
+    p2.hbar(y = desi,                    # ordonnées
+           right = round(counts),  # abscisses
+           height = 0.7,
+           fill_color=couleur[x_2],
+           line_color='black')
+    
+    st.bokeh_chart(p2)
     
     
     
-    df_code = load_df_code_designation(1)
+    
+
+    
+    
+    
+    # df_code = load_df_code_designation(1)
     
     # print(df_code.head())
     

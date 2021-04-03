d89f4da40645de4f16a9dc70ac45027ece55ce79
JulienJ-44 <74324167+JulienJ-44@users.noreply.github.com>
2021-04-03 14:26:22 +0200

    creation fontion graphiques multibar avec bokeh

--- Streamlit_rakuten/Dataviz.py
+++ Streamlit_rakuten/Dataviz.py
@@ -50,3 +50,47 @@ def get_regex_sample(val_regex):
     # class_nb.columns = ["Nombre d'articles"]
     # test = class_nb.join(df_code)
     return df_1.head(10).reset_index()
+
+@st.cache()
+def class_proba_multibar(df_ypred_proba, top=5,):
+      from bokeh.io import output_file, show
+      from bokeh.models import ColumnDataSource, FactorRange
+      from bokeh.plotting import figure
+
+      output_file("bars.html")
+
+      top_proba = df_ypred_proba.head(top)
+
+      prdct_codes = top_proba['libellé']
+      voting = top_proba['Voting']
+      rf = top_proba['RF']
+      dnn = top_proba['DNN txt']
+      cnn = top_proba['CNN img']
+
+      models = ['Voting', 'Random Forest', 'DNN Texte', 'CNN Image']
+
+      data = {'Classes' : prdct_codes,
+              'Voting'   : voting,
+              'Random Forest'   : rf,
+              'DNN Texte'   : dnn,
+              'CNN Image': cnn}
+
+      # this creates [ ("Apples", "2015"), ("Apples", "2016"), ("Apples", "2017"), ("Pears", "2015), ... ]
+      x = [ (classes, models) for code in prdct_codes for model in models ]
+      counts = sum(zip(data['Voting'], data['Random Forest'], data['DNN Texte'], data['CNN Image']), ()) # like an hstack
+
+      source = ColumnDataSource(data=dict(x=x, counts=counts))
+
+      p = figure(x_range=FactorRange(*x), plot_height=250, title="Fruit counts by year",
+                toolbar_location=None, tools="")
+
+      p.vbar(x='x', top='counts', width=0.9, source=source)
+
+      p.y_range.start = 0
+      p.x_range.range_padding = 0.1
+      p.xaxis.major_label_orientation = 1
+      p.xgrid.grid_line_color = None
+
+      return p
+
+      

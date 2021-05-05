eacde46d42deefe425cc2f66566b72f87debdd33
JulienJ-44 <74324167+JulienJ-44@users.noreply.github.com>
2021-05-05 22:08:20 +0200

    Modification visuel de l'appli

--- Streamlit_rakuten/app4.py
+++ Streamlit_rakuten/app4.py
@@ -10,22 +10,27 @@ import streamlit as st
      
 # app3.py
 def app():
+    st.title("Projet d'analyse ")
+    st.write("La mise en catalogue de listes de produits via la catégorisation des titres et des images, est un problème fondamental pour toute place de marché électronique, avec des applications allant de la recherche et recommandations personnalisées, à la compréhension des requêtes. ")
+    st.write("Dans ce contexte, le déploiement d'approches de machine learning multimodales, exploitant l’analyse conjointe des données image et texte associées aux articles, est une technique utile pour les entreprises de commerce électronique car cela permet de gagner en efficacité et précision.")
+    st.write("Dans le cadre d’un challenge organisé par l'ENS et de notre formation data scientist au sein de DataScientest, nous avons pu travailler sur la classification de produits à grande échelle, en développant un projet visant à prédire le type de chaque produit tel que défini dans le catalogue de Rakuten France.")
+    st.write("Sachant que les progrès dans ce domaine de recherche ont été limités en raison du manque de données disponibles, le fait que la société Rakuten propose un set de données exploitable a constitué une vraie opportunité.")
+    st.write("Ce projet nous a immédiatement intéressé car il permet de mettre en oeuvre de nombreux concepts de data science, tels que la manipulation de textes et d'images, la réduction de dimensions, l'utilisation de modèles de machine learning et de deep learning, ou encore le traitement de la nature bruyante des étiquettes et des images des produits. De cette manière nous avons pu concrètement expérimenter l'importance ainsi que l'impact de toutes ces étapes et méthodes liées à un projet de data science.")
+    st.write("Pour résumer, nos objectifs lors de ce projet étaient:"
+                "\n\n"
+                "•	entraîner différents modèles de machine et deep learning afin de classer parmi les différentes catégories les articles du jeu d' entraînement"
+                "\n\n"
+                "•	construire une méthode exploitant les modèles les plus performants"
+                "\n\n"
+                "•	appliquer la méthode développée sur l’ensemble de test fourni par Rakuten afin de  valider la généralisation du modèle"
+                "\n\n"
+                "•	proposer une application permettant de classer en direct tout nouvel article proposé")
 
-    st.write('')
-    st.image("./présentation/01.PNG")
-    st.write('')
-    st.image("./présentation/02.PNG")
-    st.write('')
-    st.image("./présentation/03.PNG")
-    st.write('')
-    st.image("./présentation/04.PNG")
-    st.write('')
-    st.image("./présentation/05.PNG")
-    st.write('')
-    st.image("./présentation/06.PNG")
-    st.write('')
-    st.image("./présentation/07.PNG")
-    st.write('')
-    st.image("./présentation/08.PNG")
-    st.write('')
-    st.image("./présentation/09.PNG")
+
+
+
+
+
+# 
+# "
+   
--- Streamlit_rakuten/demo_rakuten.py
+++ Streamlit_rakuten/demo_rakuten.py
@@ -10,16 +10,83 @@ import app2
 import app3
 import app4
 
+# General formating in CSS
+page_bg_img = '''
+   <style>
+    section{
+        background-color:#4db3e9;
+    color: #fff
+    }
+                       
+   h1 {
+   	color:#ffa04a
+   	
+   }
+   
+   h2 {
+   
+   color : #000000
+   	
+   }
+   
+   h3 {
+   
+   color : #ffa04a
+   	
+   }
+   
+   label {
+   color: #fff;
+   
+   ;
+   }
+   
 
+   </style>
+   '''
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
 PAGES = {
-    "Démarche": app4,
-    "Dataviz reporting": app3,
+    "Présentation du projet": app4,
+    "Exploration des données": app3,
     "Modélisation": app2,
-    "Prédiction": app1
+    "Notre démo": app1
 }
 
-st.sidebar.title('Navigation')
-selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+# st.sidebar.title('Classification d\'articles de e-commerce')
+
+
+ ## hide streamlit menu since it doesn’t really serve any purpose to the user, so there’s no reason the user should see it
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>   
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)   
+
+# Activate sidebar functions
+st.sidebar.header("Analyse de données (texte et image) d'un site de e-commerce")
+st.sidebar.header("Menu")
+
+
+
+selection = st.sidebar.radio("",list(PAGES.keys()))
 page = PAGES[selection]
 page.app()
+
+st.sidebar.info("""
+                    Projet DS - Promotion Bootcamp Janvier 2020
+
+    Participants:
+        
+    Aylin KAYA https://www.linkedin.com/in/alexis-teskrat-a879a7195/ 
+    
+    Damien LE DIRACH https://www.linkedin.com/in/f%C3%A9lix-peyre-8997b940/
+    
+    Julien JUHEL https://www.linkedin.com/in/robin-biron-48448282/
+
+    Manh NGUYEN
+                    
+                    """)
+

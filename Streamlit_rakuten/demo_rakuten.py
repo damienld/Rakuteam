d6a892627ce4e8b16be203ca97862ddeae5cf0de
JulienJ-44 <74324167+JulienJ-44@users.noreply.github.com>
2021-06-04 22:36:50 +0200

    Update style CSS dans demo_rakuten.py

--- Streamlit_rakuten/demo_rakuten.py
+++ Streamlit_rakuten/demo_rakuten.py
@@ -15,140 +15,71 @@ import app7
 # General formating in CSS
 page_bg_img = '''
    <style>
-    section{
-        background: #318CE7;        
-        color: #fff;
-    }
-                       
-   h1 {
-   	
-      color:#ffa04a ;
-   	
+
+   .main {
+       background: #318CE7; 
+       font-family:'Franklin Gothic Medium';
    }
 
-
-
-   .css-hi6a2p {
-       max-width: 830px;
-       }
+    h1, strong {
    
-   h2 {
-   
-       color: #fff;
-       
+        color:#ffa04a ; 
+    }   
 
-   	
-   }
-
-   .bk{
+    .stAlert {
+        background: #cf3;
+        color: green;
+        border: solid 1px green;
         border-radius: 15px;
-        border: solid 1px black; 
+    }
+
+     .stAlert div p {
         
-   }
+        color: green;
+      
+    }
 
-   .bk.bk-canvas-underlays{
-       background: rgba(0, 0, 255, 1);
-   }
-
-
-   img {
-    #    width: 50%;
-       margin: auto;
-       border-radius: 15px;
-       border: solid 1px black;
-
-   }
-   
-   h3 {
-   
-   color : #ffa04a;
-   	
-   }
-   
-    div>div>label {
+    .main p, li {
         color: #fff;
-   }
-
-   .st-c0{
-       color: black;
-
-   }
-
-   .st-c1{
-       color: #fff;
-
-   }
-
-   div .css-1l02zno {
-       background: #1F618D;
-       border-right: solid 3px #ffa04a
-       
-       
-   }
-
-   .css-xq1lnh-EmotionIconBase {
-    color: white;
-    fill: currentcolor;
-    font-size: 0.9rem;
-}
-
-    .st-e3 {
-        fill: black;
     }
 
-    .css-145kmo2 {
-        font-size: 0.8rem;
-        # color: rgb(38, 39, 48);
-        margin-bottom: 0.4rem;
+    .main div {
+        color: #fff;
     }
 
-   .st-dd {
-    margin: auto;
-    color: black;
-    font-style: italic;
-    font-weight: bold;
-    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    .main label {
+        color: #fff;
+    }
+
+    .element-container {
+        color: #fff;
+        font-weight: bold;
+        font-size: 1.2 em;
+    }
     
-   }
-
-   .st-ex{
-    margin: auto;
-    color: black;
-    font-style: italic;
-    font-weight: bold;
-    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    
-   }
-
-   
-   .stAlert{
-       background: #cf3;
-       border: solid 1px green;
-       border-radius: 15px;
-
-   }
-
-   pre{
-       padding: 8px;
-       border-radius: 10px;
-       margin: 50px;
-    #    background: #7d828a;
-       background: #686d75;
-       color: white;
-       font-weight: bold;
-
-   }
-
-   .css-145kmo2 {
-        font-size: 0.8rem;
-        color: #fff;
-        margin-bottom: 0.4rem;
+    pre{
+        padding: 8px;
+        border-radius: 10px;
+        margin: 50px;
+        background: #7d828a;
+        color: white;
+        font-weight: bold;
     }
 
-    .st-cc {
-        
-        color: #fff;
-        
+    .stSelectbox div {
+        color: black;
+    }
+
+    img {
+        #width: 50%;
+        margin: auto;
+        border-radius: 15px;
+        border: solid 1px black;
+    }
+
+    .bk{
+        border-radius: 15px;
+        border: solid 1px black;  
     }
 
    </style>

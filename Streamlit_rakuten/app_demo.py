b362f65eb1c3435e894ddd3e3595ad864529d739
damienld <dledirach@yahoo.fr>
2021-10-18 22:30:32 +0200

    Update app_demo.py

--- Streamlit_rakuten/app_demo.py
+++ Streamlit_rakuten/app_demo.py
@@ -138,56 +138,56 @@ def predict(desi, descr, img, clf1, scaler, inclRF, inclCNN, inclDNN):
     if (inclRF and (inclCNN and img.strip()!='') and inclDNN):
         df_ypred_proba=pd.concat([df_ypred_proba,df_code,df_ypred_proba_RF,df_ypred_proba_CNN,df_ypred_proba_DNN],axis=1)
         df_ypred_proba=df_ypred_proba.drop("Unnamed: 0", axis=1)
-        df_ypred_proba.columns=["Proba","classe","libellé","RF","CNN img","DNN txt"]
+        df_ypred_proba.columns=["Proba","Categ","Categ.Name","RF","CNN img","DNN txt"]
     elif (inclRF and (inclCNN and img.strip()!='')):    
         df_ypred_proba=pd.concat([df_ypred_proba,df_code,df_ypred_proba_RF,df_ypred_proba_CNN],axis=1)
         df_ypred_proba=df_ypred_proba.drop("Unnamed: 0", axis=1)
-        df_ypred_proba.columns=["Proba","classe","libellé","RF","CNN img"]
+        df_ypred_proba.columns=["Proba","Categ","Categ.Name","RF","CNN img"]
     elif (inclRF and inclDNN):    
         df_ypred_proba=pd.concat([df_ypred_proba,df_code,df_ypred_proba_RF,df_ypred_proba_DNN],axis=1)
         df_ypred_proba=df_ypred_proba.drop("Unnamed: 0", axis=1)
-        df_ypred_proba.columns=["Proba","classe","libellé","RF","DNN txt"]
+        df_ypred_proba.columns=["Proba","Categ","Categ.Name","RF","DNN txt"]
     elif (inclCNN and inclDNN):    
         df_ypred_proba=pd.concat([df_ypred_proba,df_code,df_ypred_proba_CNN,df_ypred_proba_DNN],axis=1)
         df_ypred_proba=df_ypred_proba.drop("Unnamed: 0", axis=1)
-        df_ypred_proba.columns=["Proba","classe","libellé","CNN img","DNN txt"]
+        df_ypred_proba.columns=["Proba","Categ","Categ.Name","CNN img","DNN txt"]
     else:    
         df_ypred_proba=pd.concat([df_ypred_proba,df_code],axis=1)
         df_ypred_proba=df_ypred_proba.drop("Unnamed: 0", axis=1)
-        df_ypred_proba.columns=["Proba","classe","libellé"]
+        df_ypred_proba.columns=["Proba","Categ","Categ.Name"]
         
     df_ypred_proba=df_ypred_proba.sort_values(by='Proba', ascending=False)
     df_ypred_proba=df_ypred_proba.reset_index()
     df_ypred_proba=df_ypred_proba.drop("index",axis=1)    
     #df_ypred_proba.sort()
     #print(df_ypred_proba.head(5))
-    st.markdown("**Classe prédite: **"+str(int(df_ypred_proba.iloc[0,1]))+" "+str(df_ypred_proba.iloc[0,2]))
+    st.markdown("**Predicted category: **"+str(int(df_ypred_proba.iloc[0,1]))+" "+str(df_ypred_proba.iloc[0,2]))
 
-    st.markdown("**Probabilités calculées par les modèles sélectionnés**")
+    st.markdown("**Probabilities calculated from the sslected models**")
     df_ypred_proba = df_ypred_proba.astype({'classe': object})
     st.dataframe(df_ypred_proba.style.highlight_max(axis=0, color=Reds))
 
     
     if (inclRF and (inclCNN and img.strip()!='') and inclDNN):
-        st.markdown("**Probabilités des 3 premières classes calculées par les différents modèles**")
+        st.markdown("**Probabilities for the 3 most likely categories (for each model)**")
         p = get_proba_bar(df_ypred_proba)
         st.bokeh_chart(p)
     
-    st.markdown("**Analyse de l'image**")
+    st.markdown("**Image Analysis**")
     p = get_colors_bar(df)
     st.bokeh_chart(p)
 
-    st.markdown("**Analyse du texte**")
+    st.markdown("**Text Analysis**")
     p = get_text_features_bar(df)
     st.bokeh_chart(p)
 
     classe_code_best_proba=int(df_ypred_proba.iloc[0,1])
     df_keywords=display_keywords_fromclasscodes(classe_code_best_proba)#,2583)
-    st.markdown("**Mots-clés de la classe prédite**")
+    st.markdown("**Keywords from the predicted category**")
     p = get_keywords_bar(df_keywords)
     st.bokeh_chart(p)
 
-    st.markdown("**Echantillon d'images de la classe prédite**")
+    st.markdown("**Sample of pictures from the predicted category**")
     path=constants.path+"echantillons/subplot_classe_" + str(int(df_ypred_proba.iloc[0,1])) +".png"
     st.image(path, width=600)
 
@@ -204,20 +204,20 @@ def app():
     file_input2=""
     #desiinit="jeu chaise longue pcs textilène noir noir"
     #descinit="cet ensemble deux chaises longues haute qualité petite table assortie idéal passer après-midi détente jardin camping chaises longues durables faciles nettoyer revêtues textilène doux confortable construites cadre acier robuste deux chaises longues d'extérieur durables résistants intempéries l'ensemble complété table assortie élégant dessus table verre lequel pouvez mettre boissons garder livre téléphone portée main cet ensemble excellent ajout espace vie extérieur couleur noir matériau chaise longue structure acier 43 siège dossier textilène dimensions chaise longue 200 58 32 cm dimensions table 30 30 295 cm hauteur dossier réglable 62/72/80/89/95 cm comprend table dessus table verre mm d'épaisseur résistance intempéries matériel polyester 30 pvc 70"
-    st.title('PREDICTION')
-    st.write("""Les différents modèles de classification peuvent être utilisés de manière indépendante ou combinés.""")
-    st.subheader("**Modèles**")
-    chkRF=st.checkbox("Random Forest: accu=0.73%, features utilisées:regexp, nb mots/phrases, moy. couleurs ...", True)
-    chkCNN=st.checkbox("CNN: accu=0.54%, feature utilisée: image de l''article", True)
-    chkDNN=st.checkbox("DNN: accu=0.82%, features utilisées: désignation et description de l'article", True)
-    st.write("""Une base d'article issue du catalogue Rakuten peut être testée de manière aléatoire.""")
-    st.write("""Mais il est aussi possible d'appliquer le modèle sur d'autres articles.""")
-    st.write("""Ils peuvent être saisis manuellement ou extraits à partir d'une url Amazon ou Vinted.""")
-    alg = ['Aléatoire','Manuel','Site: Amazon','Site: Rakuten']
-    classifier = st.selectbox('Sélection:', alg)
-    if classifier=='Site: Amazon':
+    st.title('PREDICTIONS')
+    st.write("""The 3 different models can be combinated at your own choice""")
+    st.subheader("**Models**")
+    chkRF=st.checkbox("Random Forest: accu=0.73%, features used:regexp, nb words/sentences, avg colors ...", True)
+    chkCNN=st.checkbox("CNN: accu=0.54%, feature used: image", True)
+    chkDNN=st.checkbox("DNN: accu=0.82%, features used: tile and description fields", True)
+    st.write("""Some random articles articles from the Rakuten catalog can be tested.""")
+    st.write("""But it's also possible to apply the model on some other articles.""")
+    st.write("""They can be manually inputted or extracted from an url (Amazon or Rakuten).""")
+    alg = ['Random','Manual','URL: Amazon','URL: Rakuten']
+    classifier = st.selectbox('Selection:', alg)
+    if classifier=='URL: Amazon':
         st.subheader("**Amazon**")
-        components.html('<a href="https://www.amazon.fr/gcx/Cadeaux-pour-Femmes-et-Hommes/gfhz/">Page Amazon (CTRL+click)</a>', height=25)
+        components.html('<a href="https://www.amazon.fr/gcx/Cadeaux-pour-Femmes-et-Hommes/gfhz/">Amazon Page (CTRL+click)</a>', height=25)
         url=st.text_area("URL")
         if (url != ""):
             indexref=url.find("/ref=")
@@ -225,15 +225,15 @@ def app():
                 url=url[:indexref]
             st.text(url)
             desi,descr,img,src,categ=getAmazon(url)
-            st.text_area("Page Source", src)
-            desi=st.text_area("Entrer la désignation", desi)
-            descr=st.text_area('Entrer la description', descr)
+            st.text_area("Source Page", src)
+            desi=st.text_area("Enter the title", desi)
+            descr=st.text_area('Enter the description', descr)
             if (img.strip() != ""):
                 st.image(img, width=200)
             if (categ != ""):
-                st.markdown("**Catégorie: ** "+categ) #TODO ajouter libellé classe
+                st.markdown("**Category: ** "+categ) #TODO ajouter libellé classe
             predict(desi, descr, img, clf1,scaler,chkRF,chkCNN, chkDNN)
-    elif classifier=='Site: Rakuten':
+    elif classifier=='URL: Rakuten':
         st.subheader("**Rakuten**")
         components.html('<a href="https://fr.shopping.rakuten.com/event/rakuten-deals#xtatc=PUB-[fonc]-[Header]-[Rakuten-Deals]-[ToutUnivers]-[]-[]-[]">Page Rakuten (CTRL+click)</a>', height=25)
         url=st.text_area("URL")
@@ -243,18 +243,18 @@ def app():
                 url=url[:indexref]
             st.text(url)
             desi,descr,img,src,categ=getRakuten(url)
-            st.text_area("Page Source", src)
-            desi=st.text_area("Entrer la désignation", desi)
-            descr=st.text_area('Entrer la description', descr)
+            st.text_area("Source Page", src)
+            desi=st.text_area("Enter the title", desi)
+            descr=st.text_area('Enter the description', descr)
             if (img.strip() != ""):
                 st.image(img, width=200)
             if (categ != ""):
-                st.markdown("**Catégorie: ** "+categ) #TODO ajouter libellé classe
+                st.markdown("**Category: ** "+categ) #TODO ajouter libellé classe
             predict(desi, descr, img, clf1,scaler,chkRF,chkCNN, chkDNN)
-    elif classifier=='Manuel':
-        st.subheader("**Mode Manuel**")
-        desi=st.text_area("Entrer la désignation")
-        descr=st.text_area('Entrer la description')
+    elif classifier=='Manual':
+        st.subheader("**Manual Mode**")
+        desi=st.text_area("Enter the title")
+        descr=st.text_area('Enter the description')
         file_input = st.file_uploader("Choose a image file", type="jpg")
         img=""
         if file_input is not None: 
@@ -268,13 +268,13 @@ def app():
             #st.markdown("**Classe réelle: **"+int(df_ypred_proba.iloc[0,0])+" "+str(ligne.iloc[0,1]))
 
     else:
-        st.subheader("**Mode Aléatoire**")
+        st.subheader("**Random Mode**")
         df=get_random_article()
         #st.dataframe(df)
         desi=str(df.iloc[0,0])
         descr=str(df.iloc[0,1])
-        desi=st.text_area("Entrer la désignation", desi)
-        descr=st.text_area('Entrer la description', descr)
+        desi=st.text_area("Enter the title", desi)
+        descr=st.text_area('Enter the description', descr)
         file_input2= df.iloc[0,5]
         img=constants.path+file_input2
         st.image(img)
@@ -283,8 +283,5 @@ def app():
         dataf_code_designation=load_df_code_designation()
         ligne=dataf_code_designation[dataf_code_designation["prdtypecode"]==codeclasse]
         classe_reelle_name=str(ligne.iloc[0,1])
-        st.markdown("**Classe réelle: **"+str(codeclasse)+" "+classe_reelle_name) #TODO ajouter libellé classe
+        st.markdown("**Actual category: **"+str(codeclasse)+" "+classe_reelle_name) #TODO ajouter libellé classe
         predict(desi, descr, img, clf1,scaler,chkRF,chkCNN, chkDNN)
-        
-
-        
